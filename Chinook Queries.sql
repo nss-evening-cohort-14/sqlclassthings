6fa186994612c3348b820e66a5b9823ab7d341ad
Nathan Gonzalez <ncgonz@gmail.com>
2021-09-04 11:49:44 -0500

    subqueries and union,except,intersect

--- Chinook Queries.sql
+++ Chinook Queries.sql
@@ -66,3 +66,113 @@ from Invoice
 order by Total desc --desc is for descending, asc is for ascending orders
 
 
+-------------------------------------------------------------------------------
+--------------    Subqueries    -----------------------------------------------
+-------------------------------------------------------------------------------
+--nesting one or more queries inside another one
+-- subquery, and correlated subquery are the  two main categories
+
+-- artists and their longest track
+
+-- joining to a subquery
+select *
+From Artist a
+left outer join ( select ArtistId, max(Milliseconds) LongestSongLength
+		From track t 
+			join Album a 
+				on a.AlbumId = t.AlbumId
+		group by ArtistId
+	  ) maxSong
+	on a.ArtistId = maxSong.ArtistId
+
+-- correlated subquery in the select statement
+select a.ArtistId, 
+       a.name,
+	   (select max(Milliseconds) LongestSongLength
+		From track t 
+			join Album al
+				on al.AlbumId = t.AlbumId
+		where al.ArtistId = a.ArtistId
+		group by ArtistId
+	  )
+From Artist a
+
+
+-- which artists have no tracks
+-- correlated subquery in the where clause
+select * 
+from Artist a
+where not exists (
+	select 'poop'
+	From track t 
+		join Album al
+			on al.AlbumId = t.AlbumId
+	where al.ArtistId = a.ArtistId
+)
+
+--regular subquery
+select * 
+from Artist a
+where ArtistId not in (
+	select ArtistId
+	From track t 
+		join Album al
+			on al.AlbumId = t.AlbumId
+)
+
+
+-------------------------------------------------------------------------------
+------------  UNION, EXCEPT, UNION ALL, Intersect  ----------------------------
+-------------------------------------------------------------------------------
+-- combining/comparing two or more resultsets that may or may not have anything in common
+
+
+--1,2,3,4,5,6,7,8,9,10
+--union
+--8,9,10,11,12,13,14,15
+--=
+--1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+	
+
+--1,2,3,4,5,6,7,8,9,10
+--except
+--8,9,10,11,12,13,14,15
+--=
+--1,2,3,4,5,6,7
+
+--must have the same number of columns, aliases only matter on the first set
+select Email, 'Employee' as [type]
+from Employee
+union
+select Email, 'Customer'
+from Customer
+
+
+--except operator, 
+select ArtistId
+from Artist a
+except 
+select ArtistId
+From Album
+
+--intersect, gives the middle portion of the venn diagram
+select ArtistId
+from Artist a
+intersect 
+select ArtistId
+From Album
+
+--
+select left(FirstName,1)
+from Employee
+except
+select left(FirstName,1)
+from Customer
+
+select left(FirstName,1)
+from Customer
+except
+select left(FirstName,1)
+from Employee
+
+
